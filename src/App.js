15dd1e333589aaafe04f15a308f53a40eb4b85e6
Emmanuel Olaosebikan <eolaosebikan60@gmail.com>
2022-09-23 08:18:17 +0100

    Update App.js

--- src/App.js
+++ src/App.js
@@ -1,18 +1,29 @@
 import logo from './logo.svg';
 import './App.css';
+import {useRef, useState} from 'react';
 import Header from './Components/Header';
 import Banner from './Components/Banner';
 import Tribute from './Components/Tribute';
 import Gallery from './Components/Gallery';
-
+import audio from './Components/images/audio.mpeg'
 function App() {
+    const audioRef = useRef()
+  const [play, setPlay] = useState(true)
+
+  const playAudio = ()=>{
+    if(play){
+      audioRef?.current?.play()
+    }
+  }
   return (
-    <>
+    <div onScroll={playAudio}>
       <Header />
       <Banner />
       <Gallery />
       <Tribute />
-    </>
+
+      <audio src={audio} ref={audioRef} autoPlay="" loop controls muted={false} onLoad={playAudio}/>
+    </div>
     
   );
 }
